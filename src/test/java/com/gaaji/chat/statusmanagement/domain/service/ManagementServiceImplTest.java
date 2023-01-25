525d7c0b3f5381977b90371987d018a351d262ac
Seungho <hsh47607@naver.com>
2023-01-25 23:46:58 +0900

    add test code

--- src/test/java/com/gaaji/chat/statusmanagement/domain/service/ManagementServiceImplTest.java
+++ src/test/java/com/gaaji/chat/statusmanagement/domain/service/ManagementServiceImplTest.java
@@ -1,7 +1,13 @@
 package com.gaaji.chat.statusmanagement.domain.service;
 
+import com.gaaji.chat.statusmanagement.domain.entity.ChatRoom;
+import com.gaaji.chat.statusmanagement.domain.entity.MemberId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ManagementServiceImplTest {
@@ -9,8 +15,22 @@ class ManagementServiceImplTest {
     @Autowired
     ManagementService managementService;
 
-//    @Test
-    void 채팅방_저장_구독정보_변경() {
+    @Test
+    void 채팅방_저장() {
+        // given
+        ChatRoom testRoom = ChatRoom.create("room_1", List.of("member_1", "member_2"));
+
+        // when
+        managementService.saveNewChatRoom("room_1", List.of("member_1", "member_2"));
+
+        // then
+        ChatRoom findRoom = managementService.findByRoomId("room_1");
+        Assertions.assertEquals(testRoom.getRoomId(), findRoom.getRoomId());
+        Assertions.assertEquals(testRoom.getMembers().size(), findRoom.getMembers().size());
+        Assertions.assertFalse(findRoom.getMembers().get(MemberId.of("member_1")).getIsSubscribe());
+
+        // rollback
+        managementService.deleteByRoomId(findRoom.getRoomId().getId());
     }
 
 }
