8a3bb69483848dc557398d1be6bdee089ab64cb0
Seungho <hsh47607@naver.com>
2023-01-25 15:23:53 +0900

    first commit

---
+++ src/main/java/com/gaaji/chat/statusmanagement/ChatStatusManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gaaji.chat.statusmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStatusManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatStatusManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaaji/chat/statusmanagement/ChatStatusManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.chat.statusmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStatusManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
