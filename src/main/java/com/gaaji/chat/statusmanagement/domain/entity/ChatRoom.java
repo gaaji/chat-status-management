d3af7cd826b302ec543381f8f1ac77a8158e6702
Seungho <hsh47607@naver.com>
2023-01-25 19:56:23 +0900

    feat: add new entity
    - Entity: ChatRoom (채팅방을 의미)
    - Field
      - RoomId VO
      - members (Map<MemberId, Subscribe>)
        - MemberId VO
        - Subscribe VO
    - Logic
      - create()
      - addMember()
      - updateMemberSubscribed()
      - updateMemberUnsubscribed()

---
+++ src/main/java/com/gaaji/chat/statusmanagement/domain/entity/ChatRoom.java
@@ -0,0 +1,68 @@
+package com.gaaji.chat.statusmanagement.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Data
+@RedisHash("room")
+@RequiredArgsConstructor(staticName = "of", access = AccessLevel.PRIVATE)
+public class ChatRoom implements Serializable {
+
+    @Id
+    @NonNull
+    private RoomId roomId;
+
+    @NonNull
+    private Map<MemberId, Subscribe> members;
+
+    public static ChatRoom create(String _roomId, List<String> _memberIds) {
+        RoomId roomId = RoomId.of(_roomId);
+
+        Map<MemberId, Subscribe> members = new HashMap<>();
+        _memberIds.forEach(
+                memberId -> members.put(MemberId.of(memberId), Subscribe.create()));
+
+        return ChatRoom.of(roomId, members);
+    }
+
+    public ChatRoom addMember(String memberId) {
+        members.put(MemberId.of(memberId), Subscribe.create());
+        return this;
+    }
+
+    public ChatRoom updateMemberSubscribed(String _memberId) {
+        MemberId memberId = MemberId.of(_memberId);
+        members.replace(memberId, Subscribe.subscribe());
+        return this;
+    }
+
+    public ChatRoom updateMemberUnsubscribed(String _memberId) {
+        MemberId memberId = MemberId.of(_memberId);
+        members.replace(memberId, Subscribe.unsubscribe());
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        ChatRoom cr = (ChatRoom) o;
+        return Objects.equals(this.roomId, cr.roomId);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.roomId.hashCode();
+    }
+
+}
---
+++ src/main/java/com/gaaji/chat/statusmanagement/domain/entity/MemberId.java
@@ -0,0 +1,29 @@
+package com.gaaji.chat.statusmanagement.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@RequiredArgsConstructor(staticName = "of", access = AccessLevel.PROTECTED)
+public class MemberId implements Serializable {
+
+
+    private final String id;
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        MemberId m = (MemberId) o;
+        return Objects.equals(this.id, m.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+}
