4271d028b37dad3295dbd7930e4fddf08782ccfb
Seungho <hsh47607@naver.com>
2023-01-25 20:02:49 +0900

    feat: add Redis Configuration
    - RedisConfig
      - Redis 팩토리 및 템플릿 세팅
      - 커스텀 Conversion 세팅
    - Conversions configuration
      - MemberIdConversion
        - byte[] <-> MemberId
      - RoomIdConversion
        - byte[] <-> RoomId

---
+++ src/main/java/com/gaaji/chat/statusmanagement/global/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.gaaji.chat.statusmanagement.global.config;
+
+import com.gaaji.chat.statusmanagement.global.config.converter.MemberIdConversion;
+import com.gaaji.chat.statusmanagement.global.config.converter.RoomIdConversion;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.convert.RedisCustomConversions;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+//@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisCustomConversions redisCustomConversions() {
+        List<Converter> memberIdConversions = MemberIdConversion.conversions();
+        List<Converter> roomIdConversions = RoomIdConversion.conversions();
+
+        List<Converter> converters = Stream.of(memberIdConversions, roomIdConversions)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        return new RedisCustomConversions(converters);
+    }
+
+}
---
+++ src/main/java/com/gaaji/chat/statusmanagement/global/config/converter/RoomIdConversion.java
@@ -0,0 +1,33 @@
+package com.gaaji.chat.statusmanagement.global.config.converter;
+
+import com.gaaji.chat.statusmanagement.domain.entity.RoomId;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+public class RoomIdConversion {
+
+    public static List<Converter> conversions() {
+        return List.of(new BytesToRoomIdConverter(), new RoomIdToBytesConverter());
+    }
+
+    @Component
+    @ReadingConverter
+    private static class BytesToRoomIdConverter implements Converter<byte[], RoomId> {
+        @Override
+        public RoomId convert(final byte[] source) {
+            return RoomId.of(new String(source));
+        }
+    }
+    @Component
+    @WritingConverter
+    private static class RoomIdToBytesConverter implements Converter<RoomId, byte[]> {
+        @Override
+        public byte[] convert(RoomId source) {
+            return source.getId().getBytes();
+        }
+    }
+}
