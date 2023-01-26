bd647ef789dc1ee28c6882fb8ebf3acb0db7caae
Seungho <hsh47607@naver.com>
2023-01-26 17:00:48 +0900

    feat : create NotificationController
    - KafkaListener를 통해 채팅 메시지 서버의 채팅 이벤트 발생 시, 해당 이벤트를 소비하여 처리하는 컨트롤러
    - JSON body를 DTO로 변환하여 서비스에 전달하는 역할

---
+++ src/main/java/com/gaaji/chat/statusmanagement/domain/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package com.gaaji.chat.statusmanagement.domain.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gaaji.chat.statusmanagement.domain.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @KafkaListener(topics = "chat-chatted")
+    public void sendMessageNotification(String body) throws JsonProcessingException {
+        ChattedDto chattedDto = objectMapper.readValue(body, ChattedDto.class);
+
+        notificationService.sendMessageNotification(chattedDto.getRoomId(), chattedDto.getSenderId(), chattedDto.getContent());
+    }
+}
