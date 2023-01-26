605d0cf7ba562aee42f737ade9ff9467d827e009
Seungho <hsh47607@naver.com>
2023-01-26 16:54:31 +0900

    feat: create dto class for event consume and produce

---
+++ src/main/java/com/gaaji/chat/statusmanagement/domain/controller/dto/MemberSubscribedDto.java
@@ -0,0 +1,13 @@
+package com.gaaji.chat.statusmanagement.domain.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSubscribedDto {
+    private String roomId;
+    private String memberId;
+}
