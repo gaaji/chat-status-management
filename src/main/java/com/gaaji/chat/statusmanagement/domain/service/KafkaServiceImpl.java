afed06fbc1c28f9ee9b7fc257d9e8cb772a90de5
Seungho <hsh47607@naver.com>
2023-01-26 16:56:01 +0900

    feat : create kafka service
    - method
      - sendMessageNotification()

---
+++ src/main/java/com/gaaji/chat/statusmanagement/domain/service/KafkaServiceImpl.java
@@ -0,0 +1,27 @@
+package com.gaaji.chat.statusmanagement.domain.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gaaji.chat.statusmanagement.domain.controller.ChattedDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaServiceImpl implements KafkaService{
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TOPIC_SEND_MESSAGE = "chat-chatNotified";
+
+    @Override
+    public void sendMessageNotification(ChattedDto chattedDto) throws JsonProcessingException {
+        String message = objectMapper.writeValueAsString(chattedDto);
+
+        log.info("send kafka message - topic: {}, message: {}", TOPIC_SEND_MESSAGE, message);
+        kafkaTemplate.send(TOPIC_SEND_MESSAGE, message);
+    }
+}
