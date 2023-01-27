feea725dbee8b2edb684c521c087846bbc2b2f9a
Seungho <hsh47607@naver.com>
2023-01-27 17:19:14 +0900

    feat: add openFeign

---
+++ src/main/java/com/gaaji/chat/statusmanagement/global/feign/ChatApiServiceClient.java
@@ -0,0 +1,14 @@
+package com.gaaji.chat.statusmanagement.global.feign;
+
+import com.gaaji.chat.statusmanagement.domain.controller.dto.ChatRoomCreatedDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("chat-api")
+public interface ChatApiServiceClient {
+
+    @GetMapping("/error/chat-rooms/{chatRoomId}")
+    ChatRoomCreatedDto getChatRoom(@PathVariable("chatRoomId") String roomId);
+
+}
